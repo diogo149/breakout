208ccfed8a9fe841eff9e130fa7b298bb47d0c18
Ben Moss <ben@mossity.com>
2013-02-11 21:07:18 -0500

    Pretty circles

--- src/cljs/breakout/core.cljs
+++ src/cljs/breakout/core.cljs
@@ -4,8 +4,24 @@
 (def ctx
   (.getContext (by-id "canvas") "2d"))
 
+(defn fill-style [el color]
+  (set! (.-fillStyle el) color))
+
 (doto ctx
+  (fill-style "rgba(51, 204, 255, .7)")
   (.beginPath)
-  (.arc 75 75 10 0 (* Math/PI 2) true)
+  (.arc 220 150 70 0 (* Math/PI 2) true)
+  (.closePath)
+  (.fill)
+
+  (fill-style "rgba(255, 51, 102, .5)")
+  (.beginPath)
+  (.arc 100 100 100 0 (* Math/PI 2) true)
+  (.closePath)
+  (.fill)
+
+  (fill-style "rgba(255, 204, 51, .5)")
+  (.beginPath)
+  (.rect 15 150 120 120)
   (.closePath)
   (.fill))
